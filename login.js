6dea130285f2905350381f42ab7ca35e2d4ebd46
Gabi <HunFantasztik@gmail.com>
2023-08-31 00:25:04 +0200

    Made ajax method and response disposal

--- login.js
+++ login.js
@@ -1,33 +1,53 @@
+
 let loginEventListener= document.getElementById("loginEventListener");
 
-loginEventListener.addEventListener("button", (e) =>{
-
+loginEventListener.addEventListener("submit", (e) =>{
+    
+    e.preventDefault();
+    
+    
     let email=document.getElementById("email").value;
     let password=document.getElementById("password").value;
-
-    if(email != "", password != ""){
-        let allDatas={
+    let allDatas;
+    if(email != "" && password != ""){
+        allDatas={
             email: email,
             passw: password
         }
+    }else{
+        "írjon be adatokat";
     }
+    
 
 
-    $.ajax({
 
-        url:"AdminAction.php",
-        type:"POST",
-        data:{action: "login"},
+$.ajax({
+
+    url:"Action.php",
+    type:"POST",
+    data:{action: "login", allDataKey: allDatas},
         
 
-        success: function(response){
-            if(response){
-
+    success: function(response){
+        let result = JSON.parse(response);
+        if(result["success"]){
+            if(result["loginData"]["user_type"]){
+                document.getElementById("adminNav").style.display="block";
+                document.getElementById("loginEventListener").style.display="none";
+            }else if(!result["loginData"]["user_type"]){
+                
+                document.getElementById("adminNav").style.display="block";
+                document.getElementById("loginEventListener").style.display="none";
             }
+        }else{
+            console.log(result["errorMessage"]);
         }
+        
+    },
+    error: function(xhr, status, error){
 
-    })
+    },
 
-
+    });
 
 })
