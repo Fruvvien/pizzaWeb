788a5e2b4ea362193c70e517ad5d27c21c3f5d49
Gabi <HunFantasztik@gmail.com>
2023-08-31 21:38:52 +0200

    made a SessionExist and logouts

--- Action.php
+++ Action.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+
 include_once "Queries.php";
 
 $queries = new Queries();
@@ -8,14 +9,40 @@ $queries = new Queries();
 
 
 if(isset($_POST["action"]) && $_POST["action"] == "login" && isset($_POST["allDataKey"])){
- $queriesLogin = $queries->loginUser($_POST["allDataKey"]["email"], $_POST["allDataKey"]["passw"]);
- if($queriesLogin["success"]){
-    if(/* filter_has_var(INPUT_POST,'check') &&  */!isset($_SESSION["username"])){
-        $_SESSION["username"] = $queriesLogin["result"][0]["user_name"];
-     }
-     echo json_encode(["success" => true, "loginData" => $queriesLogin, "errorMessage" => ""]);
- }else{
-    echo json_encode(["success" => false, "loginData" => [], "errorMessage" => $queriesLogin["errorMessage"]]);
+   $queriesLogin = $queries->loginUser($_POST["allDataKey"]["email"], $_POST["allDataKey"]["passw"]);
 
- } 
+
+if($queriesLogin["success"] && $queriesLogin != ""){
+   if(!isset($_SESSION["usertype"])){
+      $_SESSION["usertype"] = $queriesLogin["result"][0]["user_type"];
+   }
+      echo json_encode(["success" => true, "loginData" => $queriesLogin, "errorMessage" => ""]);
+}else{
+      echo json_encode(["success" => false, "loginData" => [], "errorMessage" => $queriesLogin["errorMessage"]]);
+
+   }  
+}
+
+
+if(isset($_POST["action"]) && $_POST["action"] == "sessionExist"){
+   $echovalue;
+   if(isset($_SESSION["usertype"])){
+      $echovalue = $_SESSION["usertype"];
+      echo json_encode(["usertype" => $echovalue, "sessionIsNotExist" => true]);
+   }else{
+      echo json_encode(["usertype" => 0, "sessionIsNotExist" => false]);
+   }
+   
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == "logoutUser"){
+   session_destroy();
+   echo true;
+     
+}
+
+if(isset($_POST["action"]) && $_POST["action"] == "logoutAdmin" ){
+   session_destroy();
+   echo true;
+  
 }
