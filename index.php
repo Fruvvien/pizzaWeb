f5f8767dfd09d03f799cec19a9a8eeecca93247b
Gabi <HunFantasztik@gmail.com>
2023-08-31 21:39:31 +0200

    seperaling user and admin

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-
-
+print_r($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,33 +13,47 @@ session_start();
     <title>Document</title>
 </head>
 <body>
+
+  
 <nav id="adminNav" class="navbar navbar-expand-lg navbar-light bg-light" style="display:none">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">AdminNavbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="#">Pizzák</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a class="nav-link" aria-current="page" href="#">Futárok</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <button class="nav-link" aria-current="page" id="logoutAdmin" >Kijelentkezés</button>
+        </li>
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+
+<nav id="user" class="navbar navbar-expand-lg navbar-light bg-light" style="display:none"> 
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">UserNavbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Pizzák</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Futárok</a>
+        </li>
+        <li class="nav-item">
+              <button class="nav-link" aria-current="page" id="logoutUser" >Kijelentkezés</button>
         </li>
       </ul>
     
@@ -52,7 +65,6 @@ session_start();
     <?php
        if(isset($_GET["page"])){
             switch($_GET["page"]){
-                
 
 
              default;
@@ -65,6 +77,6 @@ session_start();
     ?>
 </div>
 </body>
-
+<script src="session.js"></script>
 
 </html>
