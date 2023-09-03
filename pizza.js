4c604fb3cc5395f24b48d4d3120230ffd0597f6f
Gabi <HunFantasztik@gmail.com>
2023-09-03 21:22:10 +0200

    create pizza display

--- pizza.js
+++ pizza.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     pizzaPage();
 
@@ -13,7 +14,19 @@ function pizzaPage(){
 
 
         success: function(response){
-
+            let arrayParse= JSON.parse(response);
+            let pizzak="";
+            arrayParse.forEach(datas => {
+                pizzak+=
+                "<div class='card text-white bg-secondary mb-2 '>"+
+                "<div style='text-align: center' >"+datas.pnev+"</div>"+
+                "<div style='text-align: center' class='card-body'  >" +
+                "<img style='background-color: white' id='pizzaImg' src='./"+datas.url+datas.filename+"."+datas.filetype+"'>"+
+                "</div>"+
+                "<div style='text-align: center' >"+datas.par+"</div>"+
+                "</div>"
+            });
+            document.getElementById("pizza").innerHTML=pizzak;
         },
         error: function(xhr, error, errorMessage){
 
